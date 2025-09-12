a92f09b1ebc314f7c701cea7301537228c04ffd1
GuarickGit <10615@holbertonstudents.com>
2025-09-12 10:46:21 +0200

    Update | Task 6

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -56,3 +56,19 @@ console.log(createEmployee(1000));
 // Output: Director {}
 console.log(createEmployee('$500'));
 // Output: Director {}
+
+
+function isDirector(employee: Teacher | Director): employee is Director {
+	return employee instanceof Director;
+}
+
+function executeWork(employee: Teacher | Director) {
+	if (isDirector(employee)) {
+		return employee.workDirectorTasks();
+	} else {
+		return employee.workTeacherTasks();
+	}
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
